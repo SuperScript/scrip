f6f43fe8b01cf78ad9d03d43c3fe2c081c54f7b4
William Erik Baxter <web@tablethotels.com>
2015-07-24 09:41:00 -0500

    Conform to local var naming convention.

--- src/catch.sh
+++ src/catch.sh
@@ -1,11 +1,11 @@
 catch() {
-  wrap_error_re="$1"
+  catch_re="$1"
   shift
   { "$@" 2>&1 1>&9 | awk '
       BEGIN { rcode = 0 }
-      !rcode && 1 == match($0,errstring) { rcode = 111 }
+      !rcode && 1 == match($0,catch_re) { rcode = 111 }
       { print $0 }
       END { exit rcode }
-    ' errstring="$wrap_error_re" >&2
+    ' catch_re="$catch_re" >&2
   } 9>&1
 }
