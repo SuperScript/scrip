ee3d10cc823ede0a1342bbf60ab2ac0e602db7b3
William Erik Baxter <web@superscript.com>
2025-06-04 12:24:04 -0500

    Add help for do_

--- lib/do_.sh
+++ lib/do_.sh
@@ -1,5 +1,7 @@
 #include "compose.sh"
-# do_ sep prog1 [sep prog2 ...]
+#_# do_ sep prog1 [sep prog2 ...]
+#_#   Build and execute pipeline sep-separated programs
+#_#
 do_() {
   compose 'do_' "$@"
 }
