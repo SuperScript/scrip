441a4c54eb7f57f83f40d46b35588acea57d7c34
William Erik Baxter <web@superscript.com>
2025-06-04 13:46:52 -0500

    Add do_template.sh

---
+++ lib/do_template.sh
@@ -0,0 +1,21 @@
+#include "shout.sh"
+#include "barf.sh"
+#include "safe.sh"
+#include "catch.sh"
+#include "usage.sh"
+#include "have_args.sh"
+#include "do_.sh"
+#include "compose.sh"
+#include "atomic_to.sh"
+#include "atomic_to_mode.sh"
+#include "do_help.sh"
+#include "do_run.sh"
+#include "do_xrun.sh"
+
+####
+
+sep='::'
+
+####
+
+do_ "${sep}" "$@"
