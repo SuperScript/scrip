d9a704d9ca05e2d44880e4243c378d011c454c82
William Erik Baxter <web@superscript.com>
2025-08-17 22:25:46 -0500

    Add program and targets for scripting asciinema to gif

--- Makefile
+++ Makefile
@@ -1,14 +1,28 @@
 include mk/help.mk
 
+.SUFFIXES: .script .cast .gif
+
 #_# build
-#_#   The default target
+#_#   Build executables from src/*.sh
 #_#
 build:
 	SCRIP_PATH=./lib sh -c 'for f in src/*.sh; do \
-	  echo "handle $$f" >&2; \
 	  n="$${f%.sh}"; n="bin/$${n##src/}"; \
 	  bin/scrip code "$$f" > "$$n.new" && chmod a+x "$$n.new" && mv "$$n.new" "$$n"; \
 	done'
+
+#_# .script.cast
+#_#   Capture an asciinema recording of the source script
+#_#
+.script.cast: build
+	rm -f "$$(dirname "$<")/$$(basename "$<" .script).cast"
+	bin/asciinema-script cast "$$(dirname "$<")/$$(basename "$<" .script).cast" < $<
+
+#_# .cast.gif
+#_#   Create a .gif file from the .cast recording
+#_#
+.cast.gif: build
+	bin/asciinema-script gif $< "$$(dirname "$<")/$$(basename "$<" .cast).gif"
 
 #_# clean
 #_#   Remove build and test artifacts
---
+++ src/asciinema-script.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+#include "usage.sh"
+#include "do_help.sh"
+
+delay='.10s'
+font_size='18'
+
+# typeout < lines
+_typecat() {
+  awk -F'\0' -v delay="${delay}" '{
+    for (i=1;i<=length;i++) {
+      c = substr($0,i,1)
+      if (c == "\047") {
+        print "sleep " delay "; printf \047%s\047 \"\047\""
+      }
+      else {
+        print "sleep " delay "; printf \047%s\047 \047" c "\047"
+      }
+    }
+    print "printf \047\\n\047"
+  }' \
+  | sh
+}
+
+# typelines line [...]
+_typelines() {
+  printf '%s\n' "$@" | _typecat
+}
+
+# run < commands
+_run() {
+  while read x rest
+  do
+    case "$x" in
+    c|typecat)
+      _typecat "${rest}"
+      ;;
+    t|typelines)
+      _typelines "${rest}"
+      ;;
+    s|sleep)
+      sleep "${rest}"
+      ;;
+    esac
+  done
+  echo exit 0
+}
+
+#_# cast cast_file < commands
+#_#   cast script as a cast file
+#_#
+do_cast() {
+  _run | sh -c '
+    asciinema rec --cols=80 --rows=24 "$1"
+  ' asciinema "$@"
+  exit 0
+}
+
+#_# gif cast_file gif_file
+#_#   convert cast_file into gif_file with standard arguments
+#_#
+do_gif() {
+  agg --font-size "${font_size}" --cols 80 --rows 24 --fps-cap 10 --theme solarized-dark "$@"
+  exit 0
+}
+
+#### main
+
+test $# -gt 0 || usage "$0 command [args] < [inputs]"
+
+"do_$@"
+
