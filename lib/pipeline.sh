8475c86f9f786dda11cc39d8083175851c4ee5c2
William Erik Baxter <web@superscript.com>
2025-08-24 20:41:33 -0500

    Reimplement pipeline.sh

--- lib/pipeline.sh
+++ lib/pipeline.sh
@@ -1,47 +1,29 @@
-# pipeline tail-prefix prefix sep prog1 [sep prog2 ...]
-_pipeline() {
-  local tail_prefix=" $1"
-  local tail=
-  local head_prefix=" $2"
-  local head=
-  local sep="$3"
-  shift 3
-  local is_head=t
-  local a
-  local i=0
+pipeline_cmd() {
+  local prefix="$1"
+  local sep="$2"
+  shift 2
+
+  local cmd=''
+  local i=3
+  local p="${prefix}"
   for a in "$@"
   do
-    i=$(( 1 + $i ))
-    if test -n "${is_head}"
+    if test "$a" = "${sep}"
     then
-      if test "$a" = "${sep}"
-      then
-        is_head=
-        continue
-      fi
-      head="${head}\"\${$i}\" "
+      cmd="${cmd} |"
+      p="${prefix}"
     else
-      tail="${tail}\"\${$i}\" "
+      cmd="${cmd} \"${p}\${$i}\""
+      p=''
     fi
+    i=$((i + 1))
   done
-  if test -n "${tail}"
-  then
-    eval "${head_prefix}${head}" | eval "${tail_prefix} ${tail}"
-  else
-    eval "${head_prefix}${head}"
-  fi
+
+  printf '%s\n' "${cmd}"
 }
 
-# pipeline head-prefix sep prog1 [sep prog2 ...]
+# pipeline prefix sep prog1 [sep prog2 ...]
 pipeline() {
-  # NB: Global, not local
-  pipeline_nest="$((0${pipeline_nest} + 1))"
-  local f="pipeline_${pipeline_nest}"
-  eval "$f(){ _pipeline '\"$f\" ' \"$1\" \"$2\" \"\$@\" ; }"
-  shift 2
-  "$f" "$@"
-  local e=$?
-  unset -f "$f"
-  return $e
+  eval "$(pipeline_cmd "$@")"
 }
 
