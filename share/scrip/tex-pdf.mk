78cdd5a4176e4c169b9a47a60e0683b5bea3d802
William Erik Baxter <web@superscript.com>
2026-03-22 09:27:06 -0500

    Rework tex-pdf.mk atop webrows

--- share/scrip/tex-pdf.mk
+++ share/scrip/tex-pdf.mk
@@ -1,35 +1,31 @@
+.SUFFIXES: .pdf .show .tex .view
+
+listen_files := Makefile *.sty
+tex_sources != echo *.tex 2>/dev/null
+pdf_targets := $(tex_sources:.tex=.pdf)
+
 #_# TARGET.pdf
 #_#   Make PDF from .tex file in local style
 #_#
-.tex.pdf: Makefile $< *.sty
-	xelatex --shell-escape -interaction=batchmode "$<"
+.tex.pdf:
+	@xelatex --shell-escape -interaction=batchmode "$<"
+
+$(pdf_targets): Makefile *.sty
 
 #_# TARGET.show
 #_#   View TARGET.pdf in browser
 #_#
 # Attempting to preserve the current focus here fails. Why?
-.pdf.show: Makefile $<
-	firefox --class '$@' --url '$*.pdf'
+.pdf.show:
+	@webrows start >/dev/null 2>&1 || true
+	@webrows webrows get 'file://$(PWD)/$<'
 
 #_# TARGET.view
 #_#   Build TARGET.pdf from TARGET.tex and show in browser
 #_#
 # Attempting to preserve the current focus here fails. Why?
-.tex.view: Makefile $*.pdf
-	$(MAKE) '$*.show' >/dev/null 2>&1 &
-	echo '$<' | entr -np $(MAKE) '$*.update' >/dev/null 2>&1 &
-
-#_# TARGET.update
-#_#   Rebuild TARGET.pdf from TARGET.tex and refresh in browser
-#_#
-.tex.update: Makefile
-	$(MAKE) '$*.pdf'
-	xdotool search --class '$*.show' | while read w; do xdotool key --window "$$w" F5; done
-
-#_# TARGET.quit
-#_#   Quit viewing of TARGET.pdf in browser
-#_#
-.tex.quit: Makefile
-	@xdotool search --class '$*.show' | while read w; do xdotool windowclose "$$w"; done
-	@fstat '$*.tex' | awk '$$2=="entr"{print$$3}' | while read p; do kill -TERM "$$p"; done
+.tex.view:
+	@$(MAKE) '$*.pdf'
+	@$(MAKE) '$*.show'
+	@printf '%s\n' '$<' $(listen_files) | entr -np webrows webrows refresh
 
